447ed41da126440b3f04fe5b5b4cc726aacdccf4
Elmeri Niemelä <niemela.elmeri@gmail.com>
2020-05-01 13:22:27 +0300

    Add nm applet for wifi connections

--- rc.lua
+++ rc.lua
@@ -57,6 +57,10 @@ local function run_once(cmd_arr)
     end
 end
 
+run_once({
+    "nm-applet &",
+})
+
 -- run_once({ "urxvtd", "unclutter -root" }) -- entries must be separated by commas
 
 -- This function implements the XDG autostart specification
