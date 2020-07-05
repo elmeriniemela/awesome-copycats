d7a827e7af33bbe8fa54e9c01c064841d034f604
Elmeri Niemelä <niemela.elmeri@gmail.com>
2020-07-05 18:57:19 +0300

    Fix run once

--- rc.lua
+++ rc.lua
@@ -77,14 +77,14 @@ end
 -- This function will run once every time Awesome is started
 local function run_once(cmd_arr)
     for _, cmd in ipairs(cmd_arr) do
-        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s)", cmd, cmd))
+        awful.spawn.with_shell(string.format("pgrep -u $USER -fx '%s' > /dev/null || (%s &)", cmd, cmd))
     end
 end
 
 run_once({
-    "nm-applet &",
-    "xscreensaver -no-splash &",
-    "picom &",
+    "nm-applet",
+    "xscreensaver -no-splash",
+    "picom",
 })
 
 -- run_once({ "urxvtd", "unclutter -root" }) -- entries must be separated by commas
